6f4952ddd0a82a74773905871d30c829aa894f5d
Marc Pichler <marc.pichler@dynatrace.com>
2024-10-18 14:24:34 +0200

    enable logging

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import {NodeTracerProvider, SimpleSpanProcessor} from "@opentelemetry/sdk-trace-
 import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
 import {registerInstrumentations} from "@opentelemetry/instrumentation";
 import {PgInstrumentation} from "@opentelemetry/instrumentation-pg";
+import {diag, DiagConsoleLogger, DiagLogLevel} from "@opentelemetry/api";
 
 const tracerProvider = new NodeTracerProvider();
 // If you see this, don't use this as a guide, DO NOT USE SimpleSpanProcessor in production, this is for repro purposes only.
 tracerProvider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter()));
 tracerProvider.register();
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 
 registerInstrumentations({
     instrumentations: [new PgInstrumentation()]
